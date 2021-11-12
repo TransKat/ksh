e4bc3baab420bf8cee147f8f05849081416b8450
Kat <61893437+TransKat@users.noreply.github.com>
2021-11-12 17:40:26 -0500

    Add files via upload

---
+++ ksh.py
@@ -0,0 +1,23 @@
+# Kat's Shell
+
+import os
+
+# init variables
+username = os.getenv('username') # get the windows username
+pcname = os.environ['COMPUTERNAME'] # get the windows pc name
+version = "0.5.0"
+
+# print welcome text and start the loop
+clear = lambda: os.system('cls')
+clear()
+print(f"Welcome to ksh verison {version}\n\n")
+while True:
+    sinput = input(f"[{username} ~ {pcname}] ")
+    os.system(sinput)
+    if sinput == "exit":
+        print("\n * 150")
+        clear()
+        exit()
+    elif sinput == "clear":
+        print("\n * 150")
+        clear()
